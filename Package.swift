d605a96854e4716ad1d1d851f944454025df97c0
AikenCod <116162256+AikenCod@users.noreply.github.com>
2023-07-12 10:17:36 +0800

    update

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/skywinder/web3swift.git", exact: "2.6.6"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.4.3")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.7.1")
     ],
     targets: [
         .target(
